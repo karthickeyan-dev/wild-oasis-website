e279004921a62c3dc613c7c7d5799953b78d2a5b
Karthic <jkarthickeyan24@gmail.com>
2025-02-17 11:43:56 +0530

    add about, account, and cabins pages; update home page content

---
+++ app/cabins/page.js
@@ -0,0 +1,7 @@
+export default function page() {
+  return (
+    <div>
+      <h1>Cabins</h1>
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,9 +1,3 @@
 export default function Home() {
-  return (
-    <html>
-      <body>
-        <h1>Hello Next</h1>
-      </body>
-    </html>
-  );
+  return <h1>The Wild Oasis. Welcome to the Paradise</h1>;
 }
