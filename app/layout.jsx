3a9bc35bf0040685925833ae20d8274de6c43caa
Karthic <jkarthickeyan24@gmail.com>
2025-02-18 14:48:16 +0530

    add background image to Home page and enhance layout structure

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,6 @@
-import Header from "./_components/Header";
-import Logo from "./_components/Logo";
-import Navigation from "./_components/Navigation";
 import "@/app/_styles/globals.css";
 import { Josefin_Sans } from "next/font/google";
+import Header from "./_components/Header";
 
 const josefin = Josefin_Sans({
   display: "swap",
@@ -22,7 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} flex min-h-screen flex-col bg-primary-950 text-primary-100 antialiased`}
+        className={`${josefin.className} relative flex min-h-screen flex-col bg-primary-950 text-primary-100 antialiased`}
       >
         <Header />
         <div className="flex-1 px-8 py-12">
