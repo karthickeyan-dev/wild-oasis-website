6938a94c3200a2e87b9e0e2abd19912a89e21c8c
Karthic <jkarthickeyan24@gmail.com>
2025-02-18 11:50:46 +0530

    configure Prettier and Tailwind CSS

---
+++ app/layout.jsx
@@ -0,0 +1,27 @@
+import Logo from "./_components/Logo";
+import Navigation from "./_components/Navigation";
+import "@/app/_styles/globals.css";
+
+export const metadata = {
+  // title: "The Wild Oasis",
+  title: {
+    template: "%s | The Wild Oasis",
+    default: "Welcome | The Wild Oasis",
+  },
+  description: "Discover the beauty of nature with The Wild Oasis",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="min-h-screen bg-primary-950 text-primary-100">
+        <header>
+          <Logo />
+          <Navigation />
+        </header>
+        <main>{children}</main>
+        <footer>Copyright by Wild Oasis</footer>
+      </body>
+    </html>
+  );
+}
