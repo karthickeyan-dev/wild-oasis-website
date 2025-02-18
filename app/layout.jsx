7fc8bf673dedd9af10355109f26be53348c58c00
Karthic <jkarthickeyan24@gmail.com>
2025-02-18 12:01:26 +0530

    add Google font integration and apply to body class in layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,12 @@
 import Logo from "./_components/Logo";
 import Navigation from "./_components/Navigation";
 import "@/app/_styles/globals.css";
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  display: "swap",
+  subsets: ["latin"],
+});
 
 export const metadata = {
   // title: "The Wild Oasis",
@@ -14,7 +20,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="min-h-screen bg-primary-950 text-primary-100">
+      <body
+        className={`${josefin.className} min-h-screen bg-primary-950 text-primary-100`}
+      >
         <header>
           <Logo />
           <Navigation />
