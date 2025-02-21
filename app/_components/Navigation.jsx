c297b61800edce32ea5cf7370a4c268904d9c54d
Karthic <jkarthickeyan24@gmail.com>
2025-02-21 11:46:13 +0530

    enhance navigation component to display user image in account link

--- app/_components/Navigation.jsx
+++ app/_components/Navigation.jsx
@@ -1,6 +1,10 @@
 import Link from "next/link";
+import { auth } from "../_lib/auth";
+import Image from "next/image";
 
-export default function Navigation() {
+export default async function Navigation() {
+  const session = await auth();
+
   return (
     <nav className="z-10 text-xl">
       <ul className="flex items-center gap-16">
@@ -21,12 +25,29 @@ export default function Navigation() {
           </Link>
         </li>
         <li>
-          <Link
-            href="/account"
-            className="transition-colors hover:text-accent-400"
-          >
-            Guest area
-          </Link>
+          {session?.user?.image ? (
+            <Link
+              href="/account"
+              className="flex items-center gap-4 transition-colors hover:text-accent-400"
+            >
+              <Image
+                src={session.user.image}
+                alt={session.user.name}
+                width={32}
+                height={32}
+                className="size-8 rounded-full"
+                referrerPolicy="no-referrer"
+              />
+              <span>Guest area</span>
+            </Link>
+          ) : (
+            <Link
+              href="/account"
+              className="transition-colors hover:text-accent-400"
+            >
+              Guest area
+            </Link>
+          )}
         </li>
       </ul>
     </nav>
--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,13 @@ const nextConfig = {
         pathname: "/storage/v1/object/public/cabin-images/**",
         search: "",
       },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        // pathname: "/storage/v1/object/public/cabin-images/**",
+        search: "",
+      },
     ],
   },
 };
